6ea8ae00c9cafad4fff7d214006fcafe45845735
Veronica <veronica.serban08@gmail.com>
2018-01-17 23:54:56 +0200

    Added unit price text field

--- app/src/main/java/com/example/veronicas/justjava/MainActivity.java
+++ app/src/main/java/com/example/veronicas/justjava/MainActivity.java
@@ -22,6 +22,8 @@ import java.text.NumberFormat;
 public class MainActivity extends AppCompatActivity {
 
     int quantity = 2;
+    int unitPrice = 5;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,27 +35,65 @@ public class MainActivity extends AppCompatActivity {
      */
     public void increment(View view) {
         quantity = quantity + 1;
-        display(quantity);
+        display(quantity,R.id.quantity_text_view);
+
+    }
+    /**
+     * This method is called when the plus button is clicked.
+     */
+    public void incrementUnitPrice(View view) {
+        unitPrice = unitPrice + 1;
+        display(unitPrice,R.id.unitPrice_text_view);
     }
     /**
      * This method is called when the minus button is clicked.
      */
     public void decrement(View view) {
         quantity = quantity - 1;
-        display(quantity);
+        display(quantity,R.id.quantity_text_view);
+    }
+
+    /**
+     * This method is called when the minus button is clicked.
+     */
+    public void decrementUnitPrice(View view) {
+        unitPrice = unitPrice - 1;
+        display(unitPrice,R.id.unitPrice_text_view);
     }
     /**
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        displayPrice(quantity * 5);
-    }
+        int total = quantity * unitPrice;
+        String priceMessage = "Total: $ " + total;
+        priceMessage = priceMessage + "\nThank you!";
+        displayMessage(priceMessage);
 
+
+
+    }
+    /**
+     * This method displays the given text on the screen.
+     */
+    private void displayMessage(String message) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(message);
+    }
     /**
      * This method displays the given quantity value on the screen.
      */
-    private void display(int number) {
+    private void display(int number, int textViewId) {
+        TextView quantityTextView = (TextView) findViewById(textViewId);
+        quantityTextView.setText("" + number);
+    }
+
+    private void displayQ(int number) {
         TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
+        quantityTextView.setText("" + number);
+    }
+
+    private void displayP(int number) {
+        TextView quantityTextView = (TextView) findViewById(R.id.unitPrice_text_view);
         quantityTextView.setText("" + number);
     }
     /**
